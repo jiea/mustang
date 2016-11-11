91ffbe3e5898a85018263a311433b39ac80fd394
yangshunli <13366557640@163.com>
2016-11-11 15:57:35 +0800

    shunlicommit注释

--- src/test/java/com/jiea/mustang/mapper/BaseTest.java
+++ src/test/java/com/jiea/mustang/mapper/BaseTest.java
@@ -7,5 +7,5 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:spring/spring-dao.xml"})
 public class BaseTest {
-
+	//我是来测试的
 }
