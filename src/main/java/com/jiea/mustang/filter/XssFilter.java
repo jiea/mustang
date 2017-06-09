52218b958351533b6a9e5cbbe94c3850dbde9295
jiea <email_lijie@sina.com>
2017-06-09 16:58:26 +0800

    防止xss攻击

---
+++ src/main/java/com/jiea/mustang/filter/XssFilter.java
@@ -0,0 +1,26 @@
+package com.jiea.mustang.filter;
+
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class XssFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        XssHttpServletRequestWrapper requestWrapper = new XssHttpServletRequestWrapper((HttpServletRequest) request);
+        chain.doFilter(requestWrapper, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
